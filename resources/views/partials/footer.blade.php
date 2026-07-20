3a3976631d8c6f45751f48512f023807ae13ffd3
Philemon <etoundelight1@gmail.com>
2026-07-20 10:35:27 +0200

    correct

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -91,13 +91,13 @@
               WhatsApp
             </a>
           </li>
-          <li class="flex items-center gap-2 text-[14px] text-[var(--color-foreground-secondary)]">
+          {{-- <li class="flex items-center gap-2 text-[14px] text-[var(--color-foreground-secondary)]">
             <svg class="h-3.5 w-3.5 shrink-0" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z"/>
               <circle cx="12" cy="10" r="3"/>
             </svg>
             Cotonou, Bénin
-          </li>
+          </li> --}}
         </ul>
       </div>
     </div>
